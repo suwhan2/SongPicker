6c8870bb6abde05269f9b7c1a85299fb4ba3d50d
hyeon8571 <w01085914442@gmail.com>
2024-09-11 22:03:53 +0900

    🎉 백엔드 프로젝트 생성
    .

---
+++ back/Songpicker/src/main/java/com/fastarm/songpicker/SongpickerApplication.java
@@ -0,0 +1,13 @@
+package com.fastarm.songpicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongpickerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongpickerApplication.class, args);
+    }
+
+}
---
+++ back/Songpicker/src/test/java/com/fastarm/songpicker/SongpickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastarm.songpicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongpickerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
