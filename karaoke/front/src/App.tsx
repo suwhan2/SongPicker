8dfaf06605b75631e524006ac84ad4e535322a00
김민진 <kminjin99@naver.com>
2024-09-19 04:32:55 +0900

    ✨ Feat/#16: Qr 및 메인페이지 구현
    .

--- karaoke/front/src/App.tsx
+++ karaoke/front/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import LandingPage from './pages/LandingPage';
 import PayingPage from './pages/PayingPage';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     
-      <PayingPage/>
+      <MainPage/>
     
   );
 }
