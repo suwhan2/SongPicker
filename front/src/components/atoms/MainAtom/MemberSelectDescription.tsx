26874f7a1725457dfec09a6e429e892f535b8a80
조소연 <lilyjo128@naver.com>
2024-09-19 13:46:08 +0900

    feature #10 노래방 큐알연결 컴포넌트 구현

---
+++ front/src/components/atoms/MainAtom/MemberSelectDescription.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface DescriptionProps {
+  text: string;
+}
+const MemberSelectDescription = ({ text }: DescriptionProps) => {
+  return (
+    <p className="text-lg text-[#F0A9F5]">{text}</p>
+  );
+}
+
+export default MemberSelectDescription
---
+++ front/src/components/atoms/MainAtom/MemeberSelectTitle.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface TitleProps {
+  text: string;
+  className?: string;
+}
+
+const MemeberSelectTitle = ({ text, className = '' }: TitleProps) => {
+  return (
+    <div className={`text-3xl mb-2  ${className}`}>{text}</div>
+  )
+}
+
+export default MemeberSelectTitle
