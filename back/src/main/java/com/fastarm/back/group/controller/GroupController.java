fcab78e8c39fb0ed3237419cc13d00476fc6072f
suwhan2 <jerry5230@naver.com>
2024-09-23 01:19:41 +0900

    ✨ Feat/#40: 그룹 생성 기능 구현
    .

--- back/src/main/java/com/fastarm/back/group/controller/GroupController.java
+++ back/src/main/java/com/fastarm/back/group/controller/GroupController.java
@@ -2,16 +2,18 @@ package com.fastarm.back.group.controller;
 
 import com.fastarm.back.auth.security.dto.LoginMemberInfo;
 import com.fastarm.back.common.controller.dto.ApiResponse;
+import com.fastarm.back.group.controller.dto.GroupAddRequest;
+import com.fastarm.back.group.dto.GroupAddDto;
 import com.fastarm.back.group.dto.GroupDetailDto;
 import com.fastarm.back.group.dto.GroupDto;
 import com.fastarm.back.group.service.GroupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -34,6 +36,14 @@ public class GroupController {
         return ResponseEntity.ok(new ApiResponse<>("GR104", "그룹 상세 조회 성공", groupDetailDto));
     }
 
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> groupCreate(@ModelAttribute("groupImage") MultipartFile groupImage,
+                                         @ModelAttribute("groupName") String groupName,
+                                         @AuthenticationPrincipal LoginMemberInfo loginMemberInfo){
+        GroupAddRequest groupAddRequest = new GroupAddRequest(groupImage,groupName);
+        groupService.createGroup(groupAddRequest.toDto(loginMemberInfo.getLoginId()));
+        return new ResponseEntity<>(new ApiResponse<>("GR100", "그룹 생성 성공", null), HttpStatus.CREATED);
+    }
 
 
 }
--- back/src/main/java/com/fastarm/back/group/service/GroupService.java
+++ back/src/main/java/com/fastarm/back/group/service/GroupService.java
@@ -1,8 +1,11 @@
 package com.fastarm.back.group.service;
 
+import com.fastarm.back.group.controller.dto.GroupAddRequest;
+import com.fastarm.back.group.dto.GroupAddDto;
 import com.fastarm.back.group.dto.GroupDetailDto;
 import com.fastarm.back.group.dto.GroupDto;
 import com.fastarm.back.group.entity.Group;
+import com.fastarm.back.group.entity.MemberGroup;
 import com.fastarm.back.group.repository.GroupRepository;
 import com.fastarm.back.group.repository.MemberGroupRepository;
 import com.fastarm.back.member.entity.Member;
@@ -12,6 +15,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.fastarm.back.member.exception.MemberNotFoundException;
 import com.fastarm.back.group.exception.GroupNotFoundException;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -59,5 +64,24 @@ public class GroupService {
                 group.getName(),
                 members
         );
+    }
+
+    @Transactional
+    public void createGroup(GroupAddDto dto){
+        String imagePath="";
+//        String imagePath = saveImage(dto.getGroupImage()); S3 업로드 로직 추가
+        Group group = dto.toEntity(imagePath);
+        Group savedGroup = groupRepository.save(group);
+
+        Member member = memberRepository.findByLoginId(dto.getLoginId())
+                .orElseThrow(MemberNotFoundException::new);
+
+        // 방장 그룹에 자동 추가
+        MemberGroup memberGroup = MemberGroup.builder()
+                .member(member)
+                .group(savedGroup)
+                .build();
+        memberGroupRepository.save(memberGroup);
+
     }
 }
