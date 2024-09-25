f99d18d1396546814359395d8e53d91d9b70ceb0
박주영 <pjy7244@gmail.com>
2024-09-25 09:55:45 +0900

    🐛 Fix/#52: 전화번호 인증번호 수정
    .

--- front/src/stores/useAuthStore.ts
+++ front/src/stores/useAuthStore.ts
@@ -154,7 +154,7 @@ const useAuthStore = create<AuthState>()(
             requestBody.loginId = loginId;
           }
 
-          const response = await axiosInstance.post('/api/auth/phone/send', requestBody);
+          const response = await axiosInstance.post('/api/auths/phone/send', requestBody);
           const { code, message } = response.data;
           switch (code) {
             case 'AU101':
@@ -176,7 +176,7 @@ const useAuthStore = create<AuthState>()(
 
       verifyPhoneCode: async (authCode: string, phone: string) => {
         try {
-          const response = await axiosInstance.post('/api/auth/phone/verify', { authCode, phone });
+          const response = await axiosInstance.post('/api/auths/phone/verify', { authCode, phone });
           const { code, message } = response.data;
           switch (code) {
             case 'AU102':
