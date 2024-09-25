23f890e01b43dd47c1a7cb2f090b93882c7d069e
박주영 <pjy7244@gmail.com>
2024-09-25 09:26:53 +0900

    🐛 Fix/#52: 아이디 중복검사 수정
    .

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig(({ command }) => ({
           {
             src: '/src/assets/songPickerLogo.svg',
             sizes: '192x192',
-            type: 'image/svg+xml', // SVG 파일 타입 수정
+            type: 'image/svg+xml',
           },
           {
             src: '/src/assets/songPickerLogo.svg',
@@ -31,44 +31,59 @@ export default defineConfig(({ command }) => ({
           },
         ],
         theme_color: '#9747FF',
-        background_color: '#ffffff', // 배경색 추가
-        display: 'standalone', // 독립 실행형 앱처럼 표시
-        start_url: '/', // 시작 URL 설정
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'], // 오프라인 캐싱을 위한 파일 패턴
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
       },
     }),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // 소스 디렉토리에 대한 별칭 설정
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  publicDir: 'public', // 정적 파일 디렉토리 설정
+  publicDir: 'public',
 
   // 개발 서버 설정
   server: {
-    host: '0.0.0.0', // 모든 네트워크 인터페이스에서 접근 가능
-    port: 3000, // 포트 설정
+    proxy: {
+      '/api': {
+        target: 'https://songpicker.kro.kr',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+        secure: false,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      },
+    },
   },
 
-  // 빌드 최적화 설정
   build: {
-    chunkSizeWarningLimit: 1000, // 청크 크기 경고 한계 설정 (1000KB)
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return 'vendor'; // node_modules의 모듈을 별도의 청크로 분리
+            return 'vendor';
           }
         },
       },
     },
   },
 
-  // 환경별 최적화 설정
   esbuild: {
-    drop: command === 'build' ? ['console', 'debugger'] : [], // 프로덕션 빌드 시 콘솔 로그와 디버거 문 제거
+    drop: command === 'build' ? ['console', 'debugger'] : [],
   },
 }));
