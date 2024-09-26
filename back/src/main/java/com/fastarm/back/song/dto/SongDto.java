af5b0e1cb367efb500c469c24ca6ada683fe90eb
suwhan2 <jerry5230@naver.com>
2024-09-26 12:53:55 +0900

    ✨ Feat/#36: 노래 상세조회 찜기능 추가
    .

--- back/src/main/java/com/fastarm/back/song/dto/SongDto.java
+++ back/src/main/java/com/fastarm/back/song/dto/SongDto.java
@@ -1,20 +1,20 @@
 package com.fastarm.back.song.dto;
 
 import com.fastarm.back.song.entity.Song;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SongDto {
         private int number;
         private String title;
         private String singer;
         private String coverImage;
-//        private Boolean isLike;
-//        private Long likeId;
+        private Boolean isLike;
+        private Long likeId;
 
         //, boolean isLike, Long likeId
         public static SongDto from(Song song) {
