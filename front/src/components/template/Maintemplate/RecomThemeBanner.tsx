3a8942dde535235c9e973daa3a63ead69e31715d
조소연 <lilyjo128@naver.com>
2024-09-27 14:25:49 +0900

    ✨ Feat/#70: 추천테마 배너 구현
    .

--- front/src/components/template/Maintemplate/RecomThemeBanner.tsx
+++ front/src/components/template/Maintemplate/RecomThemeBanner.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaAngleRight } from 'react-icons/fa6';
+import ColMusicItem from '../../organisms/commons/ColMusicItem';
 
-type Props = {};
+type Props = {
+  title: string;
+  gradientColors: string;
+  items: Array<{
+    imageUrl: string;
+    number: string;
+    title: string;
+    artist: string;
+  }>;
+  themeLink: string;
+};
 
-const RecomThemeBanner = (props: Props) => {
-  return <div>RecomThemeBanner</div>;
+const RecomThemeBanner = ({ title, gradientColors, items, themeLink }: Props) => {
+  return (
+    <div className="mb-8">
+      <div className="flex justify-between items-center mb-3">
+        <h2 className="text-xl font-medium">{title}</h2>
+        <Link to={themeLink} className="flex items-center">
+          <p>더보기</p>
+          <FaAngleRight />
+        </Link>
+      </div>
+
+      <div className={`w-full ${gradientColors} p-3 rounded-xl flex justify-between`}>
+        {items.slice(0, 3).map((item, index) => (
+          <div key={index} className="w-[32%]">
+            <ColMusicItem {...item} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default RecomThemeBanner;
