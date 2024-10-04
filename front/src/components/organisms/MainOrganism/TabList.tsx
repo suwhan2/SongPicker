b1930e0cf29e89fec03a6cd195e28158ce2ae5be
조소연 <lilyjo128@naver.com>
2024-10-04 17:52:01 +0900

    ✨ Feat/#55: 마이리스트 모달창 디자인
    .

--- front/src/components/organisms/MainOrganism/TabList.tsx
+++ front/src/components/organisms/MainOrganism/TabList.tsx
@@ -7,18 +7,18 @@ type TabListProps = {
 
 const TabList = ({ activeTab, setActiveTab }: TabListProps) => {
   return (
-    <div className="w-full h-[60px] flex border-b">
+    <div className="w-full h-[60px] flex ">
       <button
-        className={`w-1/2 py-2 text-lg font-semibold transition-colors ${
-          activeTab === 'liked' ? 'text-white border-b-2 border-purple-500' : 'text-gray-400'
+        className={`w-1/2 py-2 text-lg font-semibold transition-colors border-b ${
+          activeTab === 'liked' ? 'text-white border-b-[3px] border-purple-500' : 'text-gray-400'
         }`}
         onClick={() => setActiveTab('liked')}
       >
         찜한 곡
       </button>
       <button
-        className={`w-1/2 py-2 text-lg font-semibold transition-colors ${
-          activeTab === 'recent' ? 'text-white border-b-2 border-purple-500' : 'text-gray-400'
+        className={`w-1/2 py-2 text-lg font-semibold transition-colors border-b ${
+          activeTab === 'recent' ? 'text-white border-b-[3px] border-purple-500' : 'text-gray-400'
         }`}
         onClick={() => setActiveTab('recent')}
       >
