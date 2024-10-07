21936b699eb608dbf76cf3667b77adc49b0b5641
조소연 <lilyjo128@naver.com>
2024-10-07 21:33:02 +0900

    ✨ Feat/#152: 테마페이지 구현
    .

---
+++ front/src/components/atoms/genre/ThemeTab.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+const ThemeTab = (props: Props) => {
+  return (
+    <div className="flex items-center justify-center py-1 px-4 bg-[#333] rounded-full">#장르</div>
+  );
+};
+
+export default ThemeTab;
