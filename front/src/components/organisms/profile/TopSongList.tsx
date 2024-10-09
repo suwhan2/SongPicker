1304428193df4cc28ba694674437830774a043df
조소연 <lilyjo128@naver.com>
2024-10-09 20:17:17 +0900

    🐛 Fix/#160: 마이페이지 css 수정
    .

--- front/src/components/organisms/profile/TopSongList.tsx
+++ front/src/components/organisms/profile/TopSongList.tsx
@@ -1,4 +1,5 @@
 import TopSongItem from '../../molecules/profile/TopSongItem';
+import { PiMicrophoneStageDuotone } from 'react-icons/pi';
 
 type TopSongListProps = {
   totalSingingCount: number;
@@ -10,14 +11,26 @@ type TopSongListProps = {
   }[];
 };
 
-const TopSongList = (props: TopSongListProps) => {
+const TopSongList = ({ totalSingingCount, topSongList }: TopSongListProps) => {
+  if (topSongList.length === 0) {
+    return (
+      <div className="w-full h-[260px] bg-[#fff] rounded-xl flex items-center justify-center flex-col">
+        <PiMicrophoneStageDuotone className="size-10 text-purple-400 mb-4" />
+        <div className="text-center text-purple-400 font-semibold">
+          <p>아직 songpicker를 이용하지 않으셨군요!</p>
+          <p>노래를 부르고 선호곡을 확인해보세요.</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col w-full bg-white text-black items-center rounded-xl p-4 gap-4">
-      {props.topSongList.map((item, i) => {
+      {topSongList.map((item, i) => {
         return (
           <TopSongItem
             topSongItem={item}
-            totalSingingCount={props.totalSingingCount}
+            totalSingingCount={totalSingingCount}
             rank={i + 1}
             key={`topSong-${item.title}-${item.singer}`}
           />
